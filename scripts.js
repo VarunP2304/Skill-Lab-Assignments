8a0bfbb006908a15977c7d51e6f30d8f3989ec9d
VarunP2304 <132540522+VarunP2304@users.noreply.github.com>
2024-07-22 23:40:26 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Add event listener for the button
+  const alertButton = document.getElementById("alertButton");
+  alertButton.addEventListener("click", function () {
+    alert("Welcome to the Paris Olympics 2024!");
+  });
+});
